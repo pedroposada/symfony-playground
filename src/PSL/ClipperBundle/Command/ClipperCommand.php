af8fa6944a4311e3cba4ca7795ef74e267e10fca
pedro.posada <pedro.posada@pslgroup.com>
2015-06-02 13:56:00 -0400

    Add parameter settings for BigCommerce. Remove commented out code. Add parameter settings for Clipper.

--- src/PSL/ClipperBundle/Command/ClipperCommand.php
+++ src/PSL/ClipperBundle/Command/ClipperCommand.php
@@ -11,7 +11,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Bigcommerce\Api\Client as Bigcommerce;
 
 // custom
-use PSL\ClipperBundle\Entity\Repository\FirstQProjectRepository;
 
 class ClipperCommand extends ContainerAwareCommand
 {
@@ -25,36 +24,51 @@ class ClipperCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-      // process bigcommerce_pending
+      $text = '';
       
-      
-      
-      // connect
-      Bigcommerce::configure(array(
-        'username'  => $this->getContainer()->getParameter('bigcommerce.username'),
-        'store_url' => $this->getContainer()->getParameter('bigcommerce.store_url'),
-        'api_key'   => $this->getContainer()->getParameter('bigcommerce.api_key')
-      ));
-      
-      // look for orders
-      $orders = array();
-      $fields = array(
-        'status_id' => $this->getContainer()->getParameter('bigcommerce.order_status_code.Completed'), // Completed
+      /**
+       * bigcommerce_pending
+       */
+      // fetch product ids from db with state 'bigcommerce_pending'
+      $repository = $this->getContainer()->get('doctrine')->getRepository('PSLClipperBundle:FirstQProject');
+      $parameters_clipper = $this->getContainer()->getParameter('clipper');
+      $fqs = $repository->findBy(
+        array('state' => $parameters_clipper['state_codes']['bigcommerce_pending'])
       );
-      $orders = Bigcommerce::getOrders($fields);
-      foreach ($orders as $order) {
-        $products = Bigcommerce::getOrderProducts($order->id);
-        foreach ($products as $product) {
-          $product_full = Bigcommerce::getProduct($product->product_id);
-          if (isset($product_full->categories) 
-            && is_array($product_full->categories) 
-            && in_array($this->getContainer()->getParameter('bigcommerce.category_code.FirstQ'), $product_full->categories)) {
-            $orders[$product_full->id] = $product_full->name;
+       
+      // connect to bigcommerce to find completed orders for these product ids
+      if (count($fqs)) {
+        $product_ids = array();
+        foreach ($fqs as $fq) {
+          $product_ids[$fq->getBcProductId()] = $fq;
+        }
+        $parameters_bigcommerce = $this->getContainer()->getParameter('bigcommerce');
+        Bigcommerce::configure(array(
+          'username'  => $parameters_bigcommerce['api']['username'],
+          'store_url' => $parameters_bigcommerce['api']['store_url'],
+          'api_key'   => $parameters_bigcommerce['api']['api_key']
+        ));
+        // look for orders by product id
+        $orders = array();
+        $fields = array(
+          'status_id' => $parameters_bigcommerce['order_status_code_completed'], // Completed orders
+        );
+        $orders = Bigcommerce::getOrders($fields);
+        foreach ($orders as $order) {
+          $products = Bigcommerce::getOrderProducts($order->id);
+          foreach ($products as $product) {
+            if (key_exists($product->product_id, $product_ids)) {
+              
+            }
           }
         }
       }
       
-      // process bigcommerce_complete
+      /**
+       * bigcommerce_complete
+       */
+      // update fqs in db
+      
       // $fq = new FirstQProject();
       // $fq->setGuid('1111');
       // $fq->setBcClientId('2222');
@@ -66,80 +80,9 @@ class ClipperCommand extends ContainerAwareCommand
       // $manager->persist($fq);
       // $manager->flush();
 
+      /**
+       * send feedback to terminal
+       */
       $output->writeln(json_encode($text));
     }
 }
-
-
-/**
- * $output = '';
-      
-      // $fq = new FirstQProject();
-      // $fq->setGuid('1111');
-      // $fq->setBcClientId('2222');
-      // $fq->setBcClientName('aaaaa');
-      // $fq->setBcProductId('3333');
-      // $fq->setFormDataRaw('4444');
-      // $fq->setSheetDataRaw('5555');
-      // $fq->setState('pending');
-      // $manager->persist($fq);
-      // $manager->flush();
-      
-      // connect
-      Bigcommerce::configure(array(
-        'username'  => 'pedro.posada',
-        'store_url' => 'https://store-th2odj.mybigcommerce.com',
-        'api_key'   => '4e3d9f3523585f0126abb48f80ac0ff544816916'
-      ));
-      
-      // add product
-      // $fields = array(
-        // 'name' => 'FirstQ Project 2',
-        // 'price' => '10',
-        // 'categories' => array(18), // FirstQ
-        // 'type' => 'digital',
-        // 'availability' => 'available',
-        // 'weight' => 0.0,
-      // );      
-      // $product = Bigcommerce::createProduct($fields);
-      
-      // get products
-      // $fields = array(
-        // // "category" => 18,
-        // "category" => 'FirstQ',
-      // );
-      // $output = Bigcommerce::getProducts($fields);
-      
-      // query orders
-      // $fqs = array();
-      // $fields = array(
-        // 'status_id' => 10, // Completed
-      // );
-      // $orders = Bigcommerce::getOrders($fields);
-      // foreach ($orders as $order) {
-        // $products = Bigcommerce::getOrderProducts($order->id);
-        // // $fqs[] = $products;
-        // foreach ($products as $product) {
-          // $fqs[] = $product->categories;
-          // // if (in_array(18, $product->categories)) {
-            // // $fqs[$product->id] = $product->name;
-          // // }
-        // }
-      // }
-      // $output = $fqs;
-      
-      
-      $fields = array(
-        'status_id' => 10, // Completed
-      );
-      $orders = Bigcommerce::getOrders($fields);
-      foreach ($orders as $order) {
-        $products = Bigcommerce::getOrderProducts($order->id);
-        foreach ($products as $product) {
-          $product_full = Bigcommerce::getProduct($product->product_id);
-          if (isset($product_full->categories) && is_array($product_full->categories) && in_array(18, $product_full->categories)) {
-            $output[$product_full->id] = $product_full->name;
-          }
-        }
-      }
- */
