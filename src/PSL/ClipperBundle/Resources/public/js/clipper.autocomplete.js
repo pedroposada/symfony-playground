6b0f4a7dc55c45698c827ec0dcf659aac5ca7fa0
pedro.posada <pedro.posada@pslgroup.com>
2015-06-27 00:39:07 -0400

    bind autocomplete to dynamically created brand inputs

--- src/PSL/ClipperBundle/Resources/public/js/clipper.autocomplete.js
+++ src/PSL/ClipperBundle/Resources/public/js/clipper.autocomplete.js
@@ -1,50 +1,38 @@
 (function($){
   $( document ).ready(function(){
     
+    // var clipperurl = "http://localhost:8000";
     var clipperurl = "http://external.dev.csb.pslgroup.com/clipper-001/web";
+    
+    var autocomplete_options = {
+      ajaxSettings: {
+        dataType: 'json',
+      },
+      paramName: 'keyword',
+      transformResult: function(response) {
+        return {
+          suggestions: $.map(response.content, function(dataItem) {
+            return { value: dataItem, data: dataItem };
+          })
+        };
+      }
+    };
     
     // styles
     // <link rel="stylesheet" href="http://external.dev.csb.pslgroup.com/clipper-001/web/bundles/pslclipper/css/jquery.autocomplete.css">
     $('head').append('<link rel="stylesheet" type="text/css" href="' + clipperurl + '/bundles/pslclipper/css/jquery.autocomplete.css">');
     
+    // conditions
+    autocomplete_options.serviceUrl = clipperurl + '/clipper/ws/clipper/autocomplete?group=conditions';
+    $('#conditions, #field_survey_patient_type').autocomplete(autocomplete_options);
+    
     // brands
-    $('#brands, .survey-brand-input').autocomplete({
-      ajaxSettings: {
-        dataType: 'json',
-      },
-      paramName: 'keyword',
-      // serviceUrl: 'http://localhost:8000/clipper/ws/clipper/autocomplete?group=brands',
-      serviceUrl: clipperurl + '/clipper/ws/clipper/autocomplete?group=brands',
-      // onSelect: function (suggestion) {
-        // alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
-      // },
-      transformResult: function(response) {
-        return {
-          suggestions: $.map(response.content, function(dataItem) {
-            return { value: dataItem, data: dataItem };
-          })
-        };
-      }
+    autocomplete_options.serviceUrl = clipperurl + '/clipper/ws/clipper/autocomplete?group=brands';
+    $('#brands, .survey-brand-input').autocomplete(autocomplete_options);
+    $('#add-brand-button').on('click', function(){
+      $('#brands, .survey-brand-input').autocomplete(autocomplete_options);
     });
     
-    // conditions
-    $('#conditions, #field_survey_patient_type').autocomplete({
-      ajaxSettings: {
-        dataType: 'json',
-      },
-      paramName: 'keyword',
-      serviceUrl: clipperurl + '/clipper/ws/clipper/autocomplete?group=conditions',
-      // onSelect: function (suggestion) {
-        // alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
-      // },
-      transformResult: function(response) {
-        return {
-          suggestions: $.map(response.content, function(dataItem) {
-            return { value: dataItem, data: dataItem };
-          })
-        };
-      }
-    });
   });
 })(jQuery);
 
