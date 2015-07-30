ff2feb6077ec67f2a0f788979463ec758779e2fe
Simon Rainville <simon.rainville@pslgroup.com>
2015-07-30 16:26:09 -0400

    rpanel_complete support of multiple combination

--- src/PSL/ClipperBundle/Listener/RpanelComplete.php
+++ src/PSL/ClipperBundle/Listener/RpanelComplete.php
@@ -17,51 +17,49 @@ class RpanelComplete extends FqProcess
 
   protected function main(FirstQProjectEvent $event)
   {
-    // get FirstQProject object
-    $fq = $event->getFirstQProject();
+    // get FirstQGroup and FirstQProject objects 
+    $fqg = $event->getFirstQProjectGroup();
+    $fqp = $event->getFirstQProject();
     
-    // @TODO: Support multi market/specialty combo
-    $ls_data = $fq->getLimesurveyDataUnserialized();
-
+    // set survey ID
+    $ls_data = $fqp->getLimesurveyDataUnserialized();
     $iSurveyID = $ls_data->sid;
-
+    
     // config connection to LS
-    $params_ls = $this->container->getParameter('limesurvey');
+    $params_ls = $this->getContainer()->getParameter('limesurvey');
     $ls = new LimeSurvey();
     $ls->configure($params_ls['api']);
-
+    
     // check if quota has been reached
     // $quota = $fq->getFormDataByField('num_participants'); // get total quota
     $quota = 1;
     $response = $ls->get_summary(array(
-      'iSurveyID' => $iSurveyID,
-      'sStatName' => 'completed_responses',
+      'iSurveyID' => $iSurveyID, 
+      'sStatName' => 'completed_responses', 
     ));
-    $this->logger->debug("completed_responses = {$response}");
-
-    if( is_array($response) && isset($response['status']) ) {
-      throw new Exception("Bad response from LimeSurvey with status [{$response['status']}] for fq->id: [{$fq->getId()}] on [get_summary]");
+    if (is_array($response) && isset($response['status'])) {
+      throw new Exception("Bad response from LimeSurvey with status [{$response['status']}] for fq->id: [{$fqp->getId()}] on [get_summary]");
     }
-
+    
     // if completed is less than quota, then exit
-    if( $quota > $response ) {
-      throw new Exception("Quota has not been reached yet for fq->id: [{$fq->getId()}]");
+    if ($quota > $response) {
+      throw new Exception("Quota has not been reached yet for fq->id: [{$fqp->getId()}]");
     }
-
+    
     // quota reached, expire survey
     $this->logger->debug("Quota ({$quota}) has been reached.", array('rpanel_complete'));
     $response = $ls->set_survey_properties(array(
-      'iSurveyID' => $iSurveyID,
-      'aSurveySettings' => array('expires' => self::$timestamp, ),
+      'iSurveyID' => $iSurveyID, 
+      'aSurveySettings' => array(
+        'expires' => self::$timestamp,
+      ), 
     ));
-
-    if( is_array($response) && isset($response['status']) ) {
-      $this->logger->debug($response['status'], array(
-        'rpanel_complete',
-        'set_survey_properties'
-      ));
-      throw new Exception("Bad response from LimeSurvey with status [{$response['status']}] for fq->id: [{$fq->getId()}] on [set_survey_properties]");
+    
+    if (is_array($response) && isset($response['status'])) {
+      $this->logger->debug($response['status'], array('rpanel_complete', 'set_survey_properties'));
+      throw new Exception("Bad response from LimeSurvey with status [{$response['status']}] for fq->id: [{$fqp->getId()}] on [set_survey_properties]");
     }
+   
 
   }
 
