853024ee35c9cfe9271c41f8c52e3fa0eb52a3c8
Simon Rainville <sim.rainville@gmail.com>
2015-08-12 15:59:55 -0400

    Added FW SSO user, user provider and password encoding, fw sso web service, and user controller for front end

---
+++ src/PSL/ClipperBundle/Controller/ClipperUserController.php
@@ -0,0 +1,257 @@
+<?php
+/**
+ * Main Clipper User Controller
+ */
+
+namespace PSL\ClipperBundle\Controller;
+
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\VarDumper;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Util\Codes;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\View\RouteRedirectView;
+use FOS\RestBundle\View\View;
+
+use PSL\ClipperBundle\Utils\FWSSOWebservices as FWSSOWebservices;
+
+use \stdClass as stdClass;
+use \Exception as Exception;
+use \DateTime as DateTime;
+use \DateTimeZone as DateTimeZone;
+
+/**
+ * Rest Controller for Clipper
+ */
+class ClipperUserController extends FOSRestController
+{
+  
+  public function __construct()
+  {
+    
+  }
+  
+  private function fwsso_ws()
+  {
+    // @TODO: get from settings
+    $settings['fwsso_baseurl'] = $this->container->getParameter('fwsso_baseurl');
+    $fwsso_ws = new FWSSOWebservice();
+    $fwsso_ws->configure($settings);
+    
+    return $fwsso_ws;
+  }
+  
+  /**
+   * ----------------------------------------------------------------------------------------
+   * API
+   * ----------------------------------------------------------------------------------------
+   */
+  
+  /**
+   * Create a FWSSO user.
+   *
+   * @ApiDoc(
+   *   resource=true,
+   *   statusCodes = {
+   *     200 = "Returned when successful",
+   *     204 = "No Content for the parameters passed"
+   *   }
+   * )
+   * 
+   * /api/users
+   *
+   * @param ParamFetcher $paramFetcher Paramfetcher
+   *
+   * @return \Symfony\Component\BrowserKit\Response
+   */
+  public function postUsersAction(ParamFetcher $paramFetcher) 
+  {
+    
+    // Object to return to remote form
+    $returnObject = array();
+    $responseStatus = 200;
+    
+    try {
+      
+      // get $_POST values
+      $user = array();
+      $user['username'] = $paramFetcher->get('username');
+      
+      // FW SSO API - create user
+      $fwsso_ws = $this->fwsso_ws();
+      
+      // do something with data
+      
+      $returnObject = $fwsso_ws->createUser($user);
+    }
+    catch (\Exception $e) {
+      // Return operation specific error
+      $returnObject['product'] = FALSE;
+      $returnObject['error_message'] =  $e->getMessage();
+      $responseStatus = 400;
+      $this->logger->debug("General exception: {$e}");
+    }
+
+    return new Response($returnObject, $responseStatus);
+  }
+  
+  /**
+   * Edit user.
+   *
+   * @ApiDoc(
+   *   resource=true,
+   *   statusCodes = {
+   *     200 = "Returned when successful",
+   *     400 = "Bad request or invalid data from the form",
+   *   }
+   * )
+   *
+   * The data is coming from an AJAX call performed on a 3rd party site
+   * 
+   * /api/users/USER_ID_XX
+   *
+   * @param ParamFetcher $paramFetcher Paramfetcher
+   * @param string $uid - Id of a FWSSO user
+   * 
+   * @return \Symfony\Component\BrowserKit\Response
+   */
+  public function postUserAction(ParamFetcher $paramFetcher, $uid)
+  {
+    
+    // Object to return to remote form
+    $returnObject = array();
+    $responseStatus = 200;
+    
+    try {
+      
+      // get $_POST values
+      $user = array();
+      $user['username'] = $paramFetcher->get('username');
+      
+      // FW SSO API - edit user info
+      
+    }
+    catch (\Exception $e) {
+      // Return operation specific error
+      $returnObject['product'] = FALSE;
+      $returnObject['error_message'] =  $e->getMessage();
+      $responseStatus = 400;
+      $this->logger->debug("General exception: {$e}");
+    }
+
+    return new Response($returnObject, $responseStatus);
+  }
+  
+  /**
+   * Retrieve a User.
+   *
+   * @ApiDoc(
+   *   resource=true,
+   *   statusCodes = {
+   *     200 = "Returned when successful",
+   *     204 = "No Content for the parameters passed"
+   *   }
+   * )
+   * 
+   * /api/users/USER_ID_XX
+   *
+   * @param Request $request the request object
+   * @param string $uid - Id of a FWSSO user
+   *
+   * @return \Symfony\Component\BrowserKit\Response
+   */
+  public function getUserAction(Request $request, $uid)
+  {
+    
+    // Object to return to remote form
+    $returnObject = array();
+    $responseStatus = 200;
+    
+    try {
+       
+      $user = array();
+      $user['id'] = $request->query->get('user_id');
+      
+      // FW SSO API - retrive info
+      
+    }
+    catch (\Exception $e) {
+      // Return operation specific error
+      $returnObject['product'] = FALSE;
+      $returnObject['error_message'] =  $e->getMessage();
+      $responseStatus = 400;
+      $this->logger->debug("General exception: {$e}");
+    }
+
+    return new Response($returnObject, $responseStatus);
+  }
+  
+  
+  /**
+   * Retrieve a User.
+   *
+   * @ApiDoc(
+   *   resource=true,
+   *   statusCodes = {
+   *     200 = "Returned when successful",
+   *     204 = "No Content for the parameters passed"
+   *   }
+   * )
+   * 
+   * /api/user/password
+   *
+   * @param Request $request the request object
+   *
+   * @return \Symfony\Component\BrowserKit\Response
+   */
+  public function getUserPasswordAction(Request $request)
+  {
+    
+    // Object to return to remote form
+    $returnObject = array();
+    $responseStatus = 200;
+    
+    try {
+      
+      $user = array();
+      $user['email'] = $request->query->get('email');
+      
+      
+      // FW SSO API - forgot password
+      
+    }
+    catch (\Exception $e) {
+      // Return operation specific error
+      $returnObject['product'] = FALSE;
+      $returnObject['error_message'] =  $e->getMessage();
+      $responseStatus = 400;
+      $this->logger->debug("General exception: {$e}");
+    }
+
+    return new Response($returnObject, $responseStatus);
+  }
+  
+  
+  /**
+   * ----------------------------------------------------------------------------------------
+   * Helper functions
+   * ----------------------------------------------------------------------------------------
+   */
+  
+  
+  
+  
+}
---
+++ src/PSL/ClipperBundle/Service/FWSSOWebservice.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace PSL\ClipperBundle\Service;
+
+use Buzz\Browser;
+
+use \Exception as Exception;
+
+/**
+ * helper class to interact with LimeSurvey
+ */
+class FWSSOWebservice
+{
+  
+  private $fwsso_baseurl;
+
+  /**
+   * Configure the API client with the required credentials.
+   *
+   * Requires a settings array to be passed in with the following keys:
+   *
+   * - fwsso_baseurl
+   *
+   * @param array $settings
+   * @throws \Exception
+   */
+  public function configure(array $settings)
+  {
+    // Validate URL
+    if (!isset($settings['fwsso_baseurl'])) {
+        throw new Exception("'fwsso_baseurl' must be provided");
+    }
+    
+    $fwsso_baseurl = $settings['fwsso_baseurl'];
+  }
+  
+  /**
+   * @return response from client
+   */
+  private function getCall($action, $param_arr) 
+  {
+    
+    $endpoint = $fwsso_baseurl . $action;
+    
+    $browser = new Browser();
+    $response = $browser->get($endpoint);
+    
+    return $response;
+  }
+  
+  /**
+   * @return response from client
+   */
+  private function postCall($action, $param_arr) 
+  {
+    
+    $endpoint = $fwsso_baseurl . $action;
+    
+    $headers = array('Content-Type'=> 'application/json');
+    
+    $content = json_encode($param_arr);
+    
+    $browser = new Browser();
+    $response = $browser->post($endpoint, $headers, $content);
+    
+    return $response;
+  }
+  
+  /**
+   * ----------------------------------------------------------------------------------------
+   * API
+   * ----------------------------------------------------------------------------------------
+   */
+  
+  /**
+   * Create a new user
+   */
+  public function createUser($param_arr = array())
+  {
+    $action = '/users';
+    
+    return $this->postCall($action, $param_arr);
+  }
+  
+  /**
+   * Edit a current user
+   */
+  public function editUser($param_arr = array())
+  {
+    $action = '/users/edit/' . $args['uid'];
+    
+    return $this->postCall($action, $param_arr);
+  }
+  
+  /**
+   * Login user
+   */
+  public function loginUser($param_arr = array())
+  {
+    $action = '/users/login';
+    
+    return $this->postCall($action, $param_arr);
+  }
+  
+  /**
+   * Forgot password
+   */
+  public function forgotPassword($param_arr = array())
+  {
+    $action = '/users/forgotpassword';
+    
+    return $this->postCall($action, $param_arr);
+  }
+  
+  /**
+   * 
+   */
+  public function getUser($param_arr = array())
+  {
+    $action = '/users/' + $param_arr['uid'];
+    
+    return $this->getCall($action, $param_arr);
+  }
+  
+  /**
+   * 
+   */
+  public function getHash($param_arr = array())
+  {
+    
+    // @TODO: make this work for realz
+    return hash('sha256', $param_arr['raw']);
+    
+    
+    $action = '/users/hash/' + $param_arr['raw'];
+    
+    return $this->getCall($action, $param_arr);
+  }
+  
+  /**
+   * ----------------------------------------------------------------------------------------
+   * Helper functions
+   * ----------------------------------------------------------------------------------------
+   */
+  
+  /**
+   * @return $this as strings
+   */
+   public function __toString()
+   {
+     return print_r($this, 1);
+   }
+  
+}
