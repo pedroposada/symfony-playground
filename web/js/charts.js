a405ca21dda9917c2ae512deb6b657faceaf1b91
pedro.posada <pedro.posada@pslgroup.com>
2015-09-09 11:40:05 -0400

    routes for charts reactjs page. reactjs logic for charts.

--- web/js/charts.js
+++ web/js/charts.js
@@ -25,6 +25,7 @@ var Comment = React.createClass({
 });
 
 var CommentBox = React.createClass({
+  
   loadCommentsFromServer: function() {
     $.ajax({
       url: this.props.url,
@@ -38,6 +39,7 @@ var CommentBox = React.createClass({
       }.bind(this)
     });
   },
+  
   handleCommentSubmit: function(comment) {
     var comments = this.state.data;
     var newComments = comments.concat([comment]);
@@ -55,13 +57,16 @@ var CommentBox = React.createClass({
       }.bind(this)
     });
   },
+  
   getInitialState: function() {
     return {data: []};
   },
+  
   componentDidMount: function() {
     this.loadCommentsFromServer();
     setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   },
+  
   render: function() {
     return (
       <div className="commentBox">
@@ -94,6 +99,7 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  
   handleSubmit: function(e) {
     e.preventDefault();
     var author = React.findDOMNode(this.refs.author).value.trim();
@@ -105,6 +111,7 @@ var CommentForm = React.createClass({
     React.findDOMNode(this.refs.author).value = '';
     React.findDOMNode(this.refs.text).value = '';
   },
+  
   render: function() {
     return (
       <form className="commentForm" onSubmit={this.handleSubmit}>
@@ -117,47 +124,149 @@ var CommentForm = React.createClass({
 });
 
 
-/* ---------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
 
 
 var chartsdata = [
-  {type: "net_promoters",   datatable: "some specific data here too"},
-  {type: "what_they_say",   datatable: "some data here"}
+  {
+    type: "BarChart", 
+    datatable: {"cols":[{"id":"b","label":"Brand","type":"string"},{"id":"P","label":"Promoters","type":"number"},{"id":"a","label":"Passives","type":"number"},{"id":"d","label":"Detractors","type":"number"},{"id":"s","label":"Score","type":"number"}],"rows":[{"c":[{"v":"AA-123"},{"v":0,"f":"0%"},{"v":0,"f":"0%"},{"v":100,"f":"100%"},{"v":-100,"f":"-100"}],"p":{"Brand":"AA-123"}},{"c":[{"v":"BB-456"},{"v":50,"f":"50%"},{"v":0,"f":"0%"},{"v":50,"f":"50%"},{"v":0,"f":"0"}],"p":{"Brand":"BB-456"}},{"c":[{"v":"CC-789"},{"v":0,"f":"0%"},{"v":0,"f":"0%"},{"v":100,"f":"100%"},{"v":-100,"f":"-100"}],"p":{"Brand":"CC-789"}},{"c":[{"v":"DD-123"},{"v":50,"f":"50%"},{"v":0,"f":"0%"},{"v":50,"f":"50%"},{"v":0,"f":"0"}],"p":{"Brand":"DD-123"}},{"c":[{"v":"EE-456"},{"v":0,"f":"0%"},{"v":50,"f":"50%"},{"v":50,"f":"50%"},{"v":-50,"f":"-50"}],"p":{"Brand":"EE-456"}},{"c":[{"v":"FF-789"},{"v":0,"f":"0%"},{"v":50,"f":"50%"},{"v":50,"f":"50%"},{"v":-50,"f":"-50"}],"p":{"Brand":"FF-789"}}]}, 
+    drilldown: { 
+      countries: ['USA', 'Canada'],
+      specialties: ['Oncology', 'Diabetes'],
+    },
+    options: {
+      isStacked: 'percent',
+      legend : {
+        position : 'top',
+        maxLines : 3
+      },
+    }
+  },
+  {
+    type: "what_they_say", 
+    datatable: "some data here"
+  }
 ];
 
+// ChartForm
+var ChartForm = React.createClass({
+  
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var country = React.findDOMNode(this.refs.country).value.trim();
+    var specialty = React.findDOMNode(this.refs.specialty).value.trim();
+    this.props.loadCharts({country: country, specialty: specialty});
+    // console.log(this.props);
+  },
+  
+  Countries: function(){
+    var output = this.props.countries.map(function(country, index){
+      return (
+        <option key={index} value={country}>{country}</option>        
+      );
+    });
+    return (
+      <select ref="country">
+        {output}
+      </select>
+    );
+  },
+  
+  Specialties: function(){
+    var output = this.props.specialties.map(function(specialty, index){
+      return (
+        <option key={index} value={specialty}>{specialty}</option>        
+      );
+    });
+    return (
+      <select ref="specialty">
+        {output}
+      </select>
+    );
+  },
+  
+  render: function() {
+    return (
+      <form className="chart-form" onSubmit={this.handleSubmit}>
+        {this.Countries()}
+        {this.Specialties()}
+        <input type="submit" value="Filter" />
+      </form>
+    );
+  }
+})
+
+// Chart
 var Chart = React.createClass({
+  
+  gsChart: function(dataTable, chartId, chartType, options){
+    google.setOnLoadCallback(drawChart);
+    var drawChart = function(dataTable) {
+      var data = new google.visualization.DataTable();
+      var chart = new google.visualization.chartType(document.getElementById(chartId));
+      chart.draw(data, options);
+    }
+  },
+  
+  
   render: function() {
     return (
       <div className="charts-chart">
-        {this.props.datatable}
+        {/* TODO: render actual chart here */}
+        
+        <div id={this.props.key}>replace with chart</div>
+        
+        { this.props.countries.length && this.props.specialties.length ? 
+          <ChartForm 
+            countries={this.props.countries} 
+            specialties={this.props.specialties}
+            loadCharts={this.props.loadCharts}
+          /> : null }
       </div>
     );
   }
 });
 
+// ChartList
 var ChartList = React.createClass({
+  
+  loadCharts: function(drilldown) {
+    var country = drilldown.country;
+    var specialty = drilldown.specialty;
+    console.log(country);
+    console.log(specialty);
+    this._getChartsData();
+  },
   
   getInitialState: function() {
     return {data: []};
   },
   
   componentDidMount: function() {
+    this._getChartsData();
+  },
+  
+  _getChartsData: function(){
     this.setState({
-      data: this.props.data || []
+      data: chartsdata || []
     });
   },
   
-  _getCharts: function(){
-    
-  },
-  
   render: function() {
-    var chartNodes = this.props.data.map(function(chart, index) {
+    var loadCharts = this.loadCharts;
+    var chartNodes = this.state.data.map(function(chart, index) {
+      var drill = chart.drilldown || {};
       return (
         <Chart 
           charttype={chart.type} 
           key={index} 
           datatable={chart.datatable} 
+          countries={drill.countries || []}
+          specialties={drill.specialties || []}
+          loadCharts={loadCharts}
         />
       );  
     });
@@ -175,12 +284,7 @@ var ChartList = React.createClass({
  * output to page
  */
 
-// React.render(
-  // // <CommentBox url="/clipper/debug" pollInterval={9999999999} />,
-  // // document.getElementById('content')
-// );
-
 React.render(
-  <ChartList data={chartsdata} />,
+  <ChartList />,
   document.getElementById('content')
 );
