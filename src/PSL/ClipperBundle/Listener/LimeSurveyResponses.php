65ff66b1238bd035e0189804124b928748442d8d
pedro.posada <pedro.posada@pslgroup.com>
2015-09-10 12:28:38 -0400

    CLIP-55 fixed undefined var PHP warning

--- src/PSL/ClipperBundle/Listener/LimeSurveyResponses.php
+++ src/PSL/ClipperBundle/Listener/LimeSurveyResponses.php
@@ -74,14 +74,14 @@ class LimeSurveyResponses
     // stop if errors from request
     if( is_array($responses) ) {
       $responses = implode(', ', $responses);
-      throw new Exception("LS export_responses error: [{$responses}] for fqp->id: [{$fqp->getId()}] and iSurveyID: [{$iSurveyID}]");
+      throw new Exception("LS export_responses error: [{$responses}] for iSurveyID: [{$iSurveyID}]");
     }
     $responses = base64_decode($responses);
     $responses = $this->serializer->decode($responses, 'json');
     
     // stop if no responses
     if (!is_array($responses['responses']) || empty($responses['responses'])) {
-      throw new Exception("LS export_responses returns empty for fqp->id: [{$fqp->getId()}] and iSurveyID: [{$iSurveyID}]");
+      throw new Exception("LS export_responses empty for iSurveyID: [{$iSurveyID}]");
     }
     
     return $responses;
