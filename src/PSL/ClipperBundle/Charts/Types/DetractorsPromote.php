e6df7c5e9746945e9709779701353ec287df1420
Ryan Harne <ryan.harne@pslgroup.com>
2015-09-22 14:25:16 +0800

    CLIP-69 & CLIP-61 [DetractorsPromote]: Additional property added for export.

--- src/PSL/ClipperBundle/Charts/Types/DetractorsPromote.php
+++ src/PSL/ClipperBundle/Charts/Types/DetractorsPromote.php
@@ -14,6 +14,10 @@ use PSL\ClipperBundle\Charts\Types\ChartType;
 class DetractorsPromote extends ChartType {
 
   private $competitors = array();
+  
+  //All responses who are detractors of the brands
+  // $answer > 0
+  private $base        = array();
 
   /**
    * Method call to return chart data.
@@ -36,6 +40,9 @@ class DetractorsPromote extends ChartType {
     //prep competitors structure
     $this->competitors = array_combine($this->brands, array_fill(0, count($this->brands), array()));
     
+    //prep base structure
+    $this->base = array_combine($this->brands, array_fill(0, count($this->brands), 0));
+    
     //stop if no responses
     if ($event->getCountFiltered()) {
       //extract respondent
@@ -55,6 +62,7 @@ class DetractorsPromote extends ChartType {
     foreach ($this->brands as $brand) {
       $dataTable[] = array(
         'brand'       => $brand,
+        'base'        => $this->base[$brand],
         'competitors' => $this->competitors[$brand]
       );
     }
@@ -87,6 +95,13 @@ class DetractorsPromote extends ChartType {
     //getting answers
     $answers = $response->getResponseDecoded();
     $answers = $this->filterAnswersToQuestionMap($answers, 'int');    
+    
+    //capture base
+    foreach ($this->brands as $brand) {
+      if ($this->identifyRespondentCategory($answers[$brand], 'detractor')) {
+        $this->base[$brand]++;
+      }
+    }
     
     $answers = array_filter($answers);
     asort($answers);
