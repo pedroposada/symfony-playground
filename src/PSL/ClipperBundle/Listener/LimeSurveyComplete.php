e44d540c765c0afa48936b6e5eb803235678c7e3
Jewei Mak <jewei.mak@pslgroup.com>
2015-10-28 01:21:16 +0800

    CLIP-75. Send email when fqg has reached state ORDER_CLOSED

--- src/PSL/ClipperBundle/Listener/LimeSurveyComplete.php
+++ src/PSL/ClipperBundle/Listener/LimeSurveyComplete.php
@@ -11,10 +11,10 @@ use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
 use PSL\ClipperBundle\Listener\FqProcess;
 use PSL\ClipperBundle\Event\FirstQProjectEvent;
+use PSL\ClipperBundle\Security\User\FWSSOQuickLoginUser;
 
 class LimeSurveyComplete extends FqProcess
 {
-
   protected function main(FirstQProjectEvent $event)
   {
     // get FirstQProject object
@@ -31,11 +31,37 @@ class LimeSurveyComplete extends FqProcess
       'iSurveyID' => $iSurveyID,
       'sHeadingType' => 'full',
     ));
+
     if( is_array($responses) ) {
       $responses = implode(', ', $responses);
       throw new Exception("LS export_responses error: [{$responses}] for fq->id: [{$fqp->getId()}] - limesurvey_complete", 2);
     }
 
+    // Email to client when quota has been reached and the report is ready.
+    // link to project report with quick-login of the user
+    $fqg = $event->getFirstQProjectGroup();
+    $user = $this->container->get('security.context')->getToken()->getUser();
+    $fwsso_quicklogin_user = new FWSSOQuickLoginUser('', $user->getUsername(), '', array());
+    $hash = $fwsso_quicklogin_user->getQuickLoginHash($this->container->getParameter('clipper.users.ql_encryptionkey'));
+    $link = $this->container->getParameter('clipper.frontend.url') . '?ql_hash=' . $hash . '&order_id=' . $fqg->getId() . '#project';
+
+    $message = \Swift_Message::newInstance()
+      ->setContentType('text/html')
+      ->setSubject('Quota has been reached')
+      ->setFrom(array('noreply@clipper.com' => 'No reply'))
+      ->setTo(array($user->getUsername()))
+      ->setBody(
+        $this->container->get('templating')->render(
+          'PSLClipperBundle:Emails:confirmation_emails.order_close.html.twig',
+          array(
+            'link' => $link,
+          ),
+          'text/html'
+        )
+      )
+    ;
+
+    $this->container->get('mailer')->send($message);
   }
 
 }
