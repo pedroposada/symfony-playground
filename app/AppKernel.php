65cee1160650f8c46e373fa69a55299af99a0acc
Alexys Hegmann <alexys.hegmann@pslgroup.com>
2016-01-08 12:40:36 -0600

    CLIP-118 - Moving from ClipperBundle resources to ClipperChartsBundle resources for charts library. Composer friendly.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,7 @@ class AppKernel extends Kernel
             new Liuggio\ExcelBundle\LiuggioExcelBundle(),
             new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
             new KG\StaticBundle\KGStaticBundle(),
+            new PSL\ClipperChartsBundle\PSLClipperChartsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
