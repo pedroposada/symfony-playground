04372992666d462fa5c87e20e00533da07e953fa
Alexys Hegmann <alexys.hegmann@pslgroup.com>
2016-01-26 16:51:51 -0600

    CLIP-128 - Moved the actual logic to rpanelcomplete because limesurveycomplete never ran to check if project time expired.

--- src/PSL/ClipperBundle/Listener/LimeSurveyComplete.php
+++ src/PSL/ClipperBundle/Listener/LimeSurveyComplete.php
@@ -22,26 +22,7 @@ class LimeSurveyComplete extends FqProcess
     $fqp = $event->getFirstQProject();
     $fqg = $event->getFirstQProjectGroup();
 
-    // CLIP-75. Project complete is either:
-    // When quota is reached OR time has expired; whichever comes first.
-    $markets = $fqg->getFormDataByField('markets');
-    $specialties = $fqg->getFormDataByField('specialties');
-    $estimated_quota = $fqg->getFormDataByField('num_participants');
-
-    $em = $this->container->get('doctrine')->getManager();
-    $responses = $em->getRepository('PSLClipperBundle:LimeSurveyResponse')->findByFirstqgroup($fqg);
-
-    $estimated_completion_date = current($fqg->getFormDataByField('completion_date'));
-
-    $now = new \DateTime('now');
-    $completion_date = new \DateTime($estimated_completion_date);
-
-    $quota_is_reached = ($estimated_quota <= count($responses));
-    $time_has_expired = ($completion_date <= $now);
-
-    if ($quota_is_reached || $time_has_expired) {
-      
-    }
+    $this->logger->debug("LimeSurvey Complete.");
   }
 
 }
